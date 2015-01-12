aada05177a97d61b3b6c241cfeedba9442d8927f
Christoph Fabianek <christoph.fabianek@gmail.com>
2015-01-12 23:21:52 +0100

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) minv <<- solve
+  getInverse <- function() solve
+  list(set = set, get = get,
+    setInverse = setInverse,
+    getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  minv <- x$getInverse()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setInverse(minv)
+  minv
 }
